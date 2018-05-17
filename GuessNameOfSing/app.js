5bdda5472a2e5f28531c946394a71380a5c21fb4
YangYoungYoung <1249091287@qq.com>
2018-05-17 10:11:43 +0800

    修改了用户授权方式

--- GuessNameOfSing/app.js
+++ GuessNameOfSing/app.js
@@ -12,9 +12,9 @@ App({
     wx.setStorageSync('logs', logs)
 
     // 登录
-    wx.login({
+    /**********wx.login({
       success: res => {
-        // 发送 res.code 到后台换取 openId, sessionKey, unionId
+        // 发送 res.code 到后台换取 openId, sessionKey, unionIdgv
         if (res.code) {
           wx.getUserInfo({
             success: function (res_user) {
@@ -42,13 +42,55 @@ App({
                   console.log('submit fail');
                 }
               })
+            },
+            fail: function (res_user){
+                                console.log('submit fail');
+              wx.showModal({
+                title: '提示',
+                content: '小程序功能需要授权才能正确使用哦！请点击“确定”-“用户信息”再次授权',
+                success: function (res) {
+                  if (res.confirm) {
+                    console.log('用户点击确定');
+                    wx.openSetting({
+                      success: (res) => {
+                        if (res.authSetting['scope.userInfo']) {
+                          var that = this;
+                          wx.request({
+                            url: 'https://xyt.xuanyutong.cn/Servlet/userLoginServlet', //登录，获得音乐值
+                            method: "POST",
+                            header: {
+                              'content-type': 'application/x-www-form-urlencoded',
+                            },
+                            data: {
+                              code: res.code,//获取openid的话 需要向后台传递code,利用code请求api获取openid
+                              avatar: res_user.userInfo.avatarUrl,//获取头像
+                              username: res_user.userInfo.nickName,
+                            },
+                            success: function (res) {
+                              // wx.setStorageSync("openid", res.data)//可以把openid保存起来,以便后期需求的使用 
+                              // console.log("返回数据为：" + res.data.aa.musicValue);//获得音乐值
+                              // console.log("返回数据为：" + res.data.aa.openid);//获得openid
+                              // console.log('submit success');
+                              wx.setStorageSync('openid', res.data.aa.openid); // 单独存储openid
+                            },
+                            fail: function () {
+                              // fail
+                              console.log('submit fail');
+                            }
+                          })
+                        }
+                      }
+                    })
+                  }
+                }
+              });
             }
           })
         }
       }
-    })
+    })************/
 
-//检测是否更新
+    // 检测是否更新
     const updateManager = wx.getUpdateManager()
     updateManager.onCheckForUpdate(function (res) {
       // 请求完新版本信息的回调
@@ -93,6 +135,5 @@ App({
   },
   globalData: {
     userInfo: null
-  },
-
+  }
 })
--- GuessNameOfSing/pages/index/index.js
+++ GuessNameOfSing/pages/index/index.js
@@ -5,63 +5,131 @@ const app = getApp()
 let hasUserInfo;
 Page({
   data: {
-    userInfo: {},
-    hasUserInfo: false,
+    //判断小程序的API，回调，参数，组件等是否在当前版本可用。
     canIUse: wx.canIUse('button.open-type.getUserInfo')
-
   },
-  startgame:function(e){
+  startgame: function (e) {
     wx.redirectTo({
       url: '../home/home',
     })
   },
   onLoad: function () {
-    //页面初始化
-    if (app.globalData.userInfo) {
-      wx.redirectTo({
-        url: '../home/home',
-      })
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true,
-      })
-    } else if (this.data.canIUse) {
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
+    var that = this
+    // 登录
+    wx.login({
+      success: res => {
+        app.globalData.code = res.code
+        //取出本地存储用户信息，解决需要每次进入小程序弹框获取用户信息
+        app.globalData.userInfo = wx.getStorageSync('userInfo')
+        //wx.getuserinfo接口不再支持
+        wx.getSetting({
+          success: (res) => {
+            //判断用户已经授权。不需要弹框
+            if (!res.authSetting['scope.userInfo']) {
+              that.setData({
+                showModel: true
+              })
+            } else {//没有授权需要弹框
+              that.setData({
+                showModel: false
+              })
+              wx.showLoading({
+                title: '加载中...'
+              })
+              that.getOP(app.globalData.userInfo)
+            }
+          },
+          fail: function () {
+            wx.showToast({
+              title: '系统提示:网络错误',
+              icon: 'warn',
+              duration: 1500,
+            })
+          }
+        })
+      },
+      fail: function () {
+        wx.showToast({
+          title: '系统提示:网络错误',
+          icon: 'warn',
+          duration: 1500,
         })
       }
+    })
+  },
+  //获取用户信息新接口
+  bindGetUserInfo: function (e) {
+    //设置用户信息本地存储
+    try {
+      wx.setStorageSync('userInfo', e.detail.userInfo)
+    } catch (e) {
+      wx.showToast({
+        title: '系统提示:网络错误',
+        icon: 'warn',
+        duration: 1500,
+      })
+    }
+    wx.showLoading({
+      title: '加载中...'
+    })
+    let that = this
+    if (e.detail.userInfo) {
+      that.getOP(e.detail.userInfo)
     } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
+      wx.showModal({
+        title: '提示',
+        content: '小程序功能需要授权才能正确使用哦！请点击“确定”-“用户信息”再次授权',
+        success: function (res) {
+          if (res.confirm) {
+            console.log('用户点击确定');
+            wx.openSetting({
+              success: (res) => {
+                if (res.authSetting['scope.userInfo']) {
+                  // that.getOP(e.detail.userInfo)
+                  wx.getUserInfo({
+                    success: res => {
+                      that.getOP(res.userInfo)
+                    }
+                  })
+                }
+              }
+            })
+          }
         }
       })
     }
   },
+  getOP: function (res) {//提交用户信息 获取用户id
+    let that = this
+    let userInfo = res
+    app.globalData.userInfo = userInfo
+    wx.request({
+      url: 'https://xyt.xuanyutong.cn/Servlet/userLoginServlet',
+      method: "POST",
+      header: {
+        'content-type': 'application/x-www-form-urlencoded',
+      },
+      data: {
+        code: app.globalData.code,//获取openid的话 需要向后台传递code,利用code请求api获取openid
+        avatar: userInfo.avatarUrl,//获取头像
+        username: userInfo.nickName,
+      },
+      success: function (res) {
+        wx.setStorageSync('openid', res.data.aa.openid); // 单独存储openid
+        wx.redirectTo({
+          url: '../home/home',
+        })
 
-  getUserInfo: function (e) {
-    //console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    if (app.globalData.userInfo) {
-      // console.log("e");
-      this.setData({
-        userInfo: e.detail.userInfo,
-        hasUserInfo: true
-      })
-      wx.redirectTo({
-        url: '../home/home',
-      })
-    }else{
-      wx.openSetting();
-    }
+      },
+      fail: function () {
+        // fail
+        console.log('submit fail');
+        wx.showToast({
+          title: '获取用户信息失败',
+          icon: 'warn',
+          duration: 1500,
+        })
+      }
+    })
   },
 })
